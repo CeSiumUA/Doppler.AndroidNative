1b0a4d0315547dd56513fe9daac98a83a0c78622
CeSiumUA <46378315+CeSiumUA@users.noreply.github.com>
2020-06-04 13:26:11 +0300

    login screen design

--- app/src/main/java/com/feddos/doppler/MainActivity.kt
+++ app/src/main/java/com/feddos/doppler/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.feddos.doppler
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feddos.doppler.authentication.Authentication
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        authButton.setOnClickListener{
+            AuthClickListener()
+        }
+    }
+    private fun AuthClickListener(){
+        if(Authentication().AuthenticateLogged(loginBox.text.toString(), passwordBox.text.toString())){
+            authResultColor.setBackgroundColor(Color.parseColor("#00ff2a"))
+
+        }
+        else{
+            authResultColor.setBackgroundColor(Color.parseColor("#fc0303"))
+        }
     }
 }
---
+++ app/src/main/java/com/feddos/doppler/authentication/auhtentication.kt
@@ -0,0 +1,8 @@
+package com.feddos.doppler.authentication
+
+public class Authentication{
+    //TODO
+    public fun AuthenticateLogged(login: String, password: String):Boolean{
+        return true;
+    }
+}
